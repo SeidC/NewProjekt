392a4d1137a42c1446e8136db7ee0e677c0912a1
SeidC <SeidelChristof@googlemail.com>
2015-11-14 16:32:33 +0100

    Start implementation of Switches

---
+++ NewProjekt/Switch/Switch.c
@@ -0,0 +1,8 @@
+/*
+ * Switch.c
+ *
+ * Created: 14.11.2015 16:30:11
+ *  Author: AP
+ */ 
+
+#include "Switch.h"
---
+++ NewProjekt/Switch/Switch.h
@@ -0,0 +1,22 @@
+/*
+ * Switch.h
+ *
+ * Created: 14.11.2015 16:29:59
+ *  Author: AP
+ */ 
+
+
+#ifndef SWITCH_H_
+#define SWITCH_H_
+
+#include "MyDefs.h"
+
+typedef struct 
+{
+	
+}Sw_Switch_t;
+
+
+
+
+#endif /* SWITCH_H_ */
