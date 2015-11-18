02128cc0df2ae439620c4f76831a2713467b69b0
SeidC <SeidelChristof@googlemail.com>
2015-11-18 23:12:56 +0100

    Testapplikation für das Modul Switch

--- NewProjekt/NewProjekt.c
+++ NewProjekt/NewProjekt.c
@@ -12,11 +12,35 @@
 
 int main(void)
 {
-        
+    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_0,USE_PIN_AS_INPUT);
+	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_1,USE_PIN_AS_INPUT);
+	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_2,USE_PIN_AS_OUTPUT);
+	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_3,USE_PIN_AS_OUTPUT);
+	
 
     while(1)
     {
-        _delay_ms(1000);
+        
+		Sw_MainFunction();
+		
+		if(Sw_GetSwitchStatus(SW_SWITCH_01) == SW_BUTTON_PUSHED)
+		{
+			Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_HIGH);
+		}
+		else
+		{
+			Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_LOW);
+		}
+		
+		if(Sw_GetSwitchStatus(SW_SWITCH_02) == SW_BUTTON_PUSHED)
+		{
+			Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_HIGH);
+		}
+		else
+		{
+			Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_LOW);
+		}
+		
         //TODO:: Please write your application code 
     }
 }
