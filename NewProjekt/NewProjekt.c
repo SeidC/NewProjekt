b40b760abbcbd357aa442255c9c7bca5051cf8b6
SeidC <SeidelChristof@googlemail.com>
2015-11-21 00:16:59 +0100

    Überarbeitung des Atmel Studio Projekts

--- NewProjekt/NewProjekt.c
+++ NewProjekt/NewProjekt.c
@@ -10,41 +10,55 @@
 #include "Switch.h"
 #include <avr/delay.h>
 
+
+void Switch_Debug(void) ;
+
+
 int main(void)
 {
-	DDRB = 0x00;
-	PORTB = 0x00; 
-	PINB  = 0x00;
-    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_0,USE_PIN_AS_INPUT);
-	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_1,USE_PIN_AS_INPUT);
-	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_2,USE_PIN_AS_OUTPUT);
-	Port_SetPinConfiguration(USE_PORT_B,USE_PIN_3,USE_PIN_AS_OUTPUT);
-	
-
-    while(1)
+	while(1)
     {
         
-		Sw_MainFunction();
-		
-		if(Sw_GetSwitchStatus(SW_SWITCH_01) == SW_BUTTON_PUSHED)
-		{
-			Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_HIGH);
-		}
-		else
-		{
-			Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_LOW);
-		}
-		
-		if(Sw_GetSwitchStatus(SW_SWITCH_02) == SW_BUTTON_PUSHED)
-		{
-			Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_HIGH);
-		}
-		else
-		{
-			Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_LOW);
-		}
-	
 	
         //TODO:: Please write your application code 
     }
+}
+
+void Switch_Debug(void) 
+{
+    DDRB = 0x00;
+    PORTB = 0x00;
+    PINB  = 0x00;
+    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_0,USE_PIN_AS_INPUT);
+    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_1,USE_PIN_AS_INPUT);
+    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_2,USE_PIN_AS_OUTPUT);
+    Port_SetPinConfiguration(USE_PORT_B,USE_PIN_3,USE_PIN_AS_OUTPUT);
+
+
+    while(1)
+    {
+    
+        Sw_MainFunction();
+    
+        if(Sw_GetSwitchStatus(SW_SWITCH_01) == SW_BUTTON_PUSHED)
+        {
+            Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_HIGH);
+        }
+        else
+        {
+            Port_SetPin(USE_PORT_B,USE_PIN_2,PIN_LOW);
+        }
+    
+        if(Sw_GetSwitchStatus(SW_SWITCH_02) == SW_BUTTON_PUSHED)
+        {
+            Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_HIGH);
+        }
+        else
+        {
+            Port_SetPin(USE_PORT_B,USE_PIN_3,PIN_LOW);
+        }
+    
+    
+        //TODO:: Please write your application code
+}
 }
