5a147e2379741fd2a1792831704996eee6fa1e02
SeidC <SeidelChristof@googlemail.com>
2015-12-04 23:26:17 +0100

    Start der Implementierung MotorControl (MC)

---
+++ NewProjekt/MC/MC.h
@@ -0,0 +1,19 @@
+/*
+ * MC.h
+ *
+ * Created: 04.12.2015 23:23:26
+ *  Author: AP
+ */ 
+
+
+#ifndef MC_H_
+#define MC_H_
+    
+#include "MyDefs.h"
+#include "Pwm.h"
+#include "MbDrv.h"
+
+
+
+
+#endif /* MC_H_ */
